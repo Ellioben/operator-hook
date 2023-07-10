d1d244eba5196cda81d66d858331ccd946ba4221
ellioben <xieyongbo@tom.com>
2023-07-10 20:02:58 +0800

    Add default hook

--- main.go
+++ main.go
@@ -96,6 +96,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ExtPower")
 		os.Exit(1)
 	}
+	if err = (&pluginv1beta1.ExtPower{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "ExtPower")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
